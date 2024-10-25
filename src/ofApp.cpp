98183c591aba87b35c946f0fd258c595a90e9210
Jessica <107456694+fungjessica@users.noreply.github.com>
2024-10-25 01:06:24 -0700

    added collision sound, complexity levels, game over

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,23 +1,56 @@
 #include "ofApp.h"
 
 
-
-
+void ofApp::setComplexityDefault(int level) {
+	switch (level) {
+	case 1: 
+		spawnRate = 1;
+		agentSpeed = 1;
+		agentLifespan = 7;
+		nAgents = 1;
+		break;
+	case 2:
+		spawnRate = 3;
+		agentSpeed = 5;
+		agentLifespan = 10;
+		nAgents = 3;
+		break;
+	case 3: 
+		spawnRate = 5;
+		agentSpeed = 10;
+		agentLifespan = 20;
+		nAgents = 5;
+		break;
+	}
+}
 //--------------------------------------------------------------
 void ofApp::setup() {
+	ofSetFrameRate(60);
+
 	bHide = false;
 	gameState = false;
+	gameOver = false;
 	textColor = ofColor::white;
 
+	background.load("images/cs134_proj2_background.png");
+	coconut.load("images/cs134_proj2_agent.png");
+	turtle.load("images/cs134_proj2_player.png");
+	particle.load("images/cs134_proj2_particle.png");
+
+	munch.load("sounds/minecraft_eating.mp3");
+
 	nEnergy = 10;
+
+	trackComplexity = complexityLevel;
 
 	gui.setup();
 	gui.add(complexityLevel.setup("Complexity Level", 1, 1, 3));
 	gui.add(playerToggleSprite.setup("Player Sprite", false));
 	gui.add(agentToggleSprite.setup("Agent Sprite", false));
-	gui.add(agentLifespan.setup("Agent Lifespan", 5, 1, 10));
+	gui.add(agentLifespan.setup("Agent Lifespan", 5, 1, 20));
 	gui.add(agentScale.setup("Agent Scale", 1, 1, 5));
-	gui.add(spawnRate.setup("Spawn Rate", 1, 1, 10));
+	gui.add(spawnRate.setup("Spawn Rate", 1, 1, 5));
+	gui.add(nAgents.setup("nAgents", 1, 1, 5));
 	gui.add(agentRotationSpeed.setup("Agent Rotation Speed", 3, 1, 10));
 	gui.add(agentSpeed.setup("Agent Speed", 1, 1, 10));
 
@@ -30,64 +63,87 @@ void ofApp::setup() {
 	gui.setPosition(10, ofGetHeight() - gui.getHeight() - 10);
 
 	emitter = Emitter();
-	//agent = Sprite();
 	emitter.pos = glm::vec3(ofGetWindowWidth() / 2.0, ofGetWindowHeight() / 2.0, 0);
 	emitter.drawable = false;
 	emitter.spriteScale = 0.5;
-
+	emitter.setRate(spawnRate);
+	emitter.setLifespan(agentLifespan * 1000);
+	emitter.nAgents = nAgents;
+	emitter.setChildImage(coconut);
+	emitter.start();
 
 	//player setup, default player sprite toggle, etc
 	playerSprite = false;
 	player.pos = glm::vec3(ofGetWidth() / 2.0, ofGetHeight() / 2.0, 0);
-
-
-	turtle.load("images/cs134_proj2_player.png");
+	player.color = ofColor::yellow;
 	player.setImage(turtle);
 
-	coconut.load("images/cs134_proj2_agent.png");
-	emitter.setChildImage(coconut);
-	emitter.start();
-
-	background.load("images/cs134 proj2 background.png");
-
+	
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-	if (gameState) {
-		// Handle complexity level
-		switch (complexityLevel) {
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
+	if (gameState && !gameOver) {
+		if (nEnergy <= 0) {
+			gameOver = true;
+			gameState = false;
 		}
 
+		// Handle complexity level
+		if (complexityLevel != trackComplexity) {
+			setComplexityDefault(complexityLevel);
+			trackComplexity = complexityLevel;
+
+			
+		}
+
+
 		// Write player methods here
-		float screenWidth = ofGetScreenWidth();
-		float screenHeight = ofGetScreenHeight();
+		player.bShowImage = playerToggleSprite;
+		player.scale = glm::vec3(float(playerScale), float(playerScale), float(playerScale));
 
 		player.integrate();
-		emitter.update();
 
-		if (player.pos.x > screenWidth) {
+		if (player.pos.x > ofGetWidth()) {
 			player.pos.x = 0;
 		}
 		if (player.pos.x < 0) {
-			player.pos.x = screenWidth;
+			player.pos.x = ofGetWidth();
 		}
-		if (player.pos.y > screenHeight) {
+		if (player.pos.y > ofGetHeight()) {
 			player.pos.y = 0;
 		}
 		if (player.pos.y < 0) {
-			player.pos.y = screenHeight;
+			player.pos.y = ofGetHeight();
 		}
 
-		// Boilerplate code for checking collisions (for images only rn)
+		if (keysPressed.count(OF_KEY_LEFT)) {
+			player.angularForce -= 300.0f;
+		}
+		if (keysPressed.count(OF_KEY_RIGHT)) {
+			player.angularForce += 300.0f;
+		}
+		if (keysPressed.count(OF_KEY_UP)) {
+			player.force += glm::vec3(glm::rotate(glm::mat4(1.0), glm::radians(player.rot), glm::vec3(0, 0, 1)) * glm::vec4(0, -1, 0, 1)) * 500;
+		}
+		if (keysPressed.count(OF_KEY_DOWN)) {
+			player.force += glm::vec3(glm::rotate(glm::mat4(1.0), glm::radians(player.rot), glm::vec3(0, 0, 1)) * glm::vec4(0, 1, 0, 1)) * 500;
+		}
+
+		
+
+		int trackAgents = emitter.sys->sprites.size();
+		if (trackAgents < nAgents) {
+			emitter.update();
+		}
+		
+
 		// Work backward so that removing does not cause skipping
 		for (int i = emitter.sys->sprites.size() - 1; i > -1; i--) {
+			emitter.sys->sprites[i].bShowImage = agentToggleSprite;
+
+			emitter.sys->sprites[i].integrate();
+
 			// Check for collisions
 			bool collision = false;
 
@@ -101,69 +157,82 @@ void ofApp::update() {
 					}
 				}
 			}
+			else {
+				for (int j = 0; j < emitter.sys->sprites[i].verts.size(); j++) {
+					if (player.inside(emitter.sys->sprites[i].getTransform() * glm::vec4(emitter.sys->sprites[i].verts[j], 1))) {
+						collision = true;
+						break;
+					}
+				}
+
+				for (int j = 0; j < player.verts.size(); j++) {
+					if (emitter.sys->sprites[i].insideTriangle(player.getTransform() * glm::vec4(player.verts[j], 1))) {
+						collision = true;
+						break;
+					}
+				}
+			}
 
 			if (collision) {
 				emitter.sys->remove(i);
+				munch.play();
+				nEnergy--;
 				continue;
 			}
 
 			// Write agent updates down here (motion, etc.)
+			glm::vec3 headingVec = glm::vec3(glm::rotate(glm::mat4(1.0), glm::radians(emitter.sys->sprites[i].rot), glm::vec3(0, 0, 1)) * glm::vec4(0, -1, 0, 1));
+			glm::vec3 toPlayerVec = glm::normalize(player.pos - emitter.sys->sprites[i].pos);
+			emitter.sys->sprites[i].force += headingVec * (float)agentSpeed * 50;
+			float angle = glm::degrees(glm::orientedAngle(headingVec, toPlayerVec, glm::vec3(0, 0, 1)));
+			if (angle < 0.01)
+				emitter.sys->sprites[i].angularForce -= 250;
+			else
+				emitter.sys->sprites[i].angularForce += 250;
 
-
+			emitter.setRate(spawnRate);
+			emitter.setLifespan(agentLifespan * 1000);
+			
 		}
 	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-	ofSetColor(255, 255, 255);
-	background.draw(0, 0, ofGetWidth(), ofGetHeight());
-
-	// Draw gui
-	if (!bHide)
-		gui.draw();
+	ofSetColor(textColor);
+	float scale = max(ofGetWidth() / background.getWidth(), ofGetHeight() / background.getHeight());
+	float width = background.getWidth() * scale;
+	float height = background.getHeight() * scale;
+	background.draw((ofGetWidth() - width) / 2, (ofGetHeight() - height) / 2, width, height);
 
 	// Draw depending on gameState (started, stopped)
 	if (gameState) {
 		endTime = ofGetElapsedTimef();
-		ofSetColor(textColor);
 		ofDrawBitmapString("FPS: " + ofToString(ofGetFrameRate()), 10, 20);
 		ofDrawBitmapString("Energy: " + ofToString(nEnergy) + "/" + ofToString(10), 10, 40);
 		ofDrawBitmapString("Time: " + ofToString(endTime - startTime), 10, 60);
 
+		player.draw();
+		emitter.draw();
 
-		//toggle between triangle and turtle
-		if (playerToggleSprite) {
-			player.bShowImage = true;
-			ofSetColor(255, 255, 255);
-			player.draw();
-		}
-		else {
-			player.bShowImage = false;
-			player.draw();
-		}
-		if (agentToggleSprite) {
-			emitter.haveChildImage = true;
-			emitter.setChildImage(coconut);
-			emitter.draw();
-		}
-		else {
-			emitter.haveChildImage = false;
-			emitter.draw();
-		}
-
-
+	}
+	else if (gameOver) {
+		ofSetColor(ofColor::red);
+		ofDrawBitmapString("Game Over", ofGetWidth() / 2 - 40, ofGetHeight() / 2);
+		ofDrawBitmapString("Press Space to Restart", ofGetWidth() / 2 - 80, ofGetHeight() / 2 + 20);
+		
 	}
 	else {
 		ofBitmapFont font = ofBitmapFont();
 		string text = (nEnergy > 0) ? "Press Space to Start" : "Last Record: " + ofToString(endTime - startTime) + "\nPress Space to Start";
 		int width = font.getBoundingBox(text, 0, 0).getWidth();
 		int height = font.getBoundingBox(text, 0, 0).getHeight();
-		ofSetColor(textColor);
 		ofDrawBitmapString(text, ofGetWidth() / 2 - width / 2, ofGetHeight() / 2 - height / 2);
 	}
 
-
+	// Draw gui
+	if (!bHide)
+		gui.draw();
 }
 
 //--------------------------------------------------------------
@@ -179,29 +248,20 @@ void ofApp::keyPressed(int key) {
 		ofToggleFullscreen();
 		break;
 	case ' ':
-		if (gameState == false) {
+		if (gameOver) {
+			nEnergy = 10;
+			gameOver = false;
+			gameState = false;
+			complexityLevel = 1;
+			playerToggleSprite = false;
+			agentToggleSprite = false;
+			startTime = ofGetElapsedTimef();
+		}
+		else if (gameState == false) {
 			gameState = true;
 			startTime = ofGetElapsedTimef();
 		}
 		break;
-	}
-
-	if (keysPressed.count(OF_KEY_LEFT)) {
-		player.angularForce -= 300.0f;
-	}
-	if (keysPressed.count(OF_KEY_RIGHT)) {
-		player.angularForce += 300.0f;
-	}
-	if (keysPressed.count(OF_KEY_UP)) {
-		float forceX = cos(glm::radians(player.rot)) * -500.0f;
-		float forceY = sin(glm::radians(player.rot)) * -500.0f;
-		player.force += glm::vec3(forceX, forceY, 0);
-		player.integrate();
-	}
-	if (keysPressed.count(OF_KEY_DOWN)) {
-		float forceX = cos(glm::radians(player.rot)) * 500.0f;
-		float forceY = sin(glm::radians(player.rot)) * 500.0f;
-		player.force += glm::vec3(forceX, forceY, 0);
 	}
 }
 
